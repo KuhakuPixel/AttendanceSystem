913d88c35cbe843496f383b027dc76dce60b50e5
kuhakupixel <nicholaspixels@gmail.com>
2025-07-09 15:06:55 +0700

    add admin

--- setup.sql
+++ setup.sql
@@ -6,7 +6,7 @@ CREATE TABLE Users (
 	username VARCHAR(50) NOT NULL UNIQUE,
 	age INT,
 	password VARCHAR(255) NOT NULL,
-	is_admin BOOLEAN DEFAULT FALSE
+	is_admin BOOLEAN DEFAULT FALSE NOT NULL
 );
 
 CREATE TABLE UserAttendance (
@@ -16,3 +16,5 @@ CREATE TABLE UserAttendance (
     photo_proof BLOB NOT NULL,
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
 );
+
+INSERT INTO Users(email, username, age, password, is_admin) VALUES("admin@gmail.com", "admin", 30, "adminpassword", TRUE);
