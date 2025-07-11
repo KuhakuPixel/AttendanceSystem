5621ef0e781d8f67ee498142e37b98a928b1033d
kuhakupixel <nicholaspixels@gmail.com>
2025-07-11 08:51:25 +0700

    add db connection

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,32 @@ const express = require('express')
 const app = express()
 const port = 3005
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mysql = require('mysql2/promise');
 
-app.listen(port, (error) => {
-  console.log(`Example app listening on port ${port}`)
-  console.log(`error: ${error}`)
-})
+(async function () {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'password',
+    database: 'db'
+  })
+
+  await connection.connect()
+
+  const [rows, fields] = await connection.query('SELECT 1 + 1 AS solution');
+  console.log('The solution is: ', rows[0].solution)
+
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.post('/register-admin', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.listen(port, (error) => {
+    console.log(`Example app listening on port ${port}`)
+    console.log(`error: ${error}`)
+  })
+})();
