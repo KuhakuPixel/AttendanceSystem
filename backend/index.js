a950cb326385aca7f466afaa51bd2bc51ee85bff
kuhakupixel <nicholaspixels@gmail.com>
2025-07-11 21:05:11 +0700

    add update user

--- backend/index.js
+++ backend/index.js
@@ -1,110 +1,177 @@
-
-const TOKEN_LENGTH = 128;
+const TOKEN_LENGTH = 128
 const express = require('express')
 const app = express()
-const HttpStatus = require('http-status-codes');
-const crypto = require("crypto");
-const id = crypto.randomBytes(20).toString('hex');
+const HttpStatus = require('http-status-codes')
+const crypto = require('crypto')
+const id = crypto.randomBytes(20).toString('hex')
 const port = 3005
 
-const mysql = require('mysql2/promise');
-var bodyParser = require('body-parser');
-const { User, UserAttendance, UserSession } = require('./model');
-const { requireLogin } = require('./auth');
+const mysql = require('mysql2/promise')
+var bodyParser = require('body-parser')
+const { User, UserAttendance, UserSession } = require('./model')
+const { requireLogin } = require('./auth')
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+  ; (async function () {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'user',
+      password: 'password',
+      database: 'db'
+    })
 
-(async function () {
-  const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'user',
-    password: 'password',
-    database: 'db'
-  })
+    await connection.connect()
 
-  await connection.connect()
+    const [rows, fields] = await connection.query('SELECT 1 + 1 AS solution')
+    console.log('The solution is: ', rows[0].solution)
 
-  const [rows, fields] = await connection.query('SELECT 1 + 1 AS solution');
-  console.log('The solution is: ', rows[0].solution)
+    app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
 
+    app.post('/register-admin', async (req, res) => {
+      console.log(req.body)
+      let user = new User(
+        req.body['username'],
+        req.body['email'],
+        req.body['age'],
+        req.body['password'],
+        true
+      )
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-  app.post('/register-admin', async (req, res) => {
-    console.log(req.body);
-    let user = new User(
-      req.body["username"],
-      req.body["email"],
-      req.body["age"],
-      req.body["password"],
-      true
-    );
-
-    try {
-      await user.save(connection);
-      res.send('account created');
-    }
-    catch (error) {
-      res.status(HttpStatus.StatusCodes.BAD_REQUEST).send(error["message"]);
-    }
-  })
-
-  app.post('/register-employee', async (req, res) => {
-    res.send('Hello World!')
-    console.log(req.body);
-    let user = new User(
-      req.body["username"],
-      req.body["email"],
-      req.body["age"],
-      req.body["password"],
-      false
-    );
-
-    try {
-      await user.save(connection);
-      res.send('account created');
-    }
-    catch (error) {
-      res.status(HttpStatus.StatusCodes.BAD_REQUEST).send(error["message"]);
-    }
-  })
-
-  app.post('/login', async (req, res) => {
-    try {
-      let user = await User.getUserByEmail(connection, req.body["email"]);
-      if (user.password != req.body["password"]) {
-        res.send("wrong password");
-
-      } else {
-        let token = crypto.randomBytes(TOKEN_LENGTH).toString('hex');
-        let user_session = new UserSession(user.id, token);
-        user_session.save(connection);
-        res.send(token);
+      try {
+        await user.save(connection)
+        res.send('account created')
+      } catch (error) {
+        res.status(HttpStatus.StatusCodes.BAD_REQUEST).send(error['message'])
       }
-    }
-    catch (error) {
-      console.log("error is " + error);
-      res.status(HttpStatus.StatusCodes.BAD_REQUEST).send(error.toString());
-    }
-  })
+    })
 
-  app.get('/attendances', async (req, res) => {
-    let user = null;
-    try {
-      user = await requireLogin(connection, req, true);
-    }
-    catch (error) {
-      res.status(HttpStatus.StatusCodes.UNAUTHORIZED).send(error.toString());
-      return;
-    }
-    res.send('Hello World!')
-  })
+    app.post('/register-employee', async (req, res) => {
+      res.send('Hello World!')
+      console.log(req.body)
+      let user = new User(
+        req.body['username'],
+        req.body['email'],
+        req.body['age'],
+        req.body['password'],
+        false
+      )
 
-  app.listen(port, (error) => {
-    console.log(`Example app listening on port ${port}`)
-    console.log(`error: ${error}`)
-  })
-})();
+      try {
+        await user.save(connection)
+        res.send('account created')
+      } catch (error) {
+        res.status(HttpStatus.StatusCodes.BAD_REQUEST).send(error['message'])
+      }
+    })
+
+    app.post('/login', async (req, res) => {
+      try {
+        let user = await User.getUserByEmail(connection, req.body['email'])
+        if (user.password != req.body['password']) {
+          res.send('wrong password')
+        } else {
+          let token = crypto.randomBytes(TOKEN_LENGTH).toString('hex')
+          let user_session = new UserSession(user.id, token)
+          user_session.save(connection)
+          res.send(token)
+        }
+      } catch (error) {
+        console.log('error is ' + error)
+        res.status(HttpStatus.StatusCodes.BAD_REQUEST).send(error.toString())
+      }
+    })
+
+    app.get('/users', async (req, res) => {
+      let user = null
+      try {
+        user = await requireLogin(connection, req, true)
+      } catch (error) {
+        res.status(HttpStatus.StatusCodes.UNAUTHORIZED).send(error.toString())
+        return
+      }
+
+      const users = await User.getAll(connection)
+      console.log('users ' + users)
+      res.send(users)
+    })
+
+    app.put('/users/:id', async (req, res) => {
+      let user = null
+      try {
+        user = await requireLogin(connection, req, true)
+      } catch (error) {
+        res.status(HttpStatus.StatusCodes.UNAUTHORIZED).send(error.toString())
+        return
+      }
+
+      let user_to_update = null
+      try {
+        user_to_update = await User.getUserById(connection, req.params['id'])
+      } catch (error) {
+        res.status(HttpStatus.StatusCodes.BAD_REQUEST).send(error.toString())
+        return
+      }
+      console.log(user_to_update)
+      Object.assign(user_to_update, req.body)
+      await user_to_update.save(connection)
+      res.send(user_to_update);
+    })
+
+    app.get('/attendances', async (req, res) => {
+      let user = null
+      try {
+        user = await requireLogin(connection, req, true)
+      } catch (error) {
+        res.status(HttpStatus.StatusCodes.UNAUTHORIZED).send(error.toString())
+        return
+      }
+      res.send('Hello World!')
+    })
+
+    app.post('/checkin', async (req, res) => {
+      let user = null
+      try {
+        user = await requireLogin(connection, req, false)
+      } catch (error) {
+        res.status(HttpStatus.StatusCodes.UNAUTHORIZED).send(error.toString())
+        return
+      }
+      var d = new Date()
+      let user_attendance = new UserAttendance(user.id, d, '', 'checkin')
+      await user_attendance.save(connection)
+      res.send('ok')
+    })
+
+    app.post('/checkout', async (req, res) => {
+      let user = null
+      try {
+        user = await requireLogin(connection, req, false)
+      } catch (error) {
+        res.status(HttpStatus.StatusCodes.UNAUTHORIZED).send(error.toString())
+        return
+      }
+      var d = new Date()
+      let user_attendance = new UserAttendance(user.id, d, '', 'checkout')
+      await user_attendance.save(connection)
+      res.send('ok')
+    })
+
+    app.listen(port, error => {
+      console.log(`Example app listening on port ${port}`)
+      console.log(`error: ${error}`)
+    })
+  })()
+/**
+ * TODO:
+ * - api for making attendances
+ * - view attendances
+ *
+ * - delete/update employee data
+ *
+ */
+//
+//
+//
--- backend/model.js
+++ backend/model.js
@@ -1,60 +1,87 @@
 class User {
     constructor(username, email, age, password, is_admin) {
-        this.username = username;
-        this.email = email;
-        this.age = age;
-        this.id = "";
-        this.password = password;
-        this.is_admin = is_admin;
+        this.username = username
+        this.email = email
+        this.age = age
+        this.id = null
+        this.password = password
+        this.is_admin = is_admin
     }
 
     async save(db_con) {
         // execute will internally call prepare and query
-        const [results, fields] = await db_con.execute(
-            'INSERT INTO Users(email, username, age, password, is_admin) VALUES(?, ?, ?, ?, ?)',
-            [this.email, this.username, this.age, this.password, this.is_admin]
-        );
-        console.log(results)
+        if (this.id == null) {
+            const [results, fields] = await db_con.execute(
+                'INSERT INTO Users(email, username, age, password, is_admin) VALUES(?, ?, ?, ?, ?)',
+                [this.email, this.username, this.age, this.password, this.is_admin]
+            )
+            console.log(results)
+        }
+        else {
+            const [results, fields] = await db_con.execute(
+                'UPDATE Users SET email = ?, username = ?, age = ?, password = ?, is_admin = ? WHERE id = ?',
+                [this.email, this.username, this.age, this.password, this.is_admin, this.id]
+            )
+
+            console.log(results)
+        }
+    }
+
+    static async getAll(db_con) {
+        const [results, fields] = await db_con.execute('SELECT * FROM Users')
+        return results
     }
     static async getUserByEmail(db_con, email) {
         const [results, fields] = await db_con.execute(
             'SELECT * FROM Users WHERE email = ?',
             [email]
-        );
+        )
         if (results.length == 0) {
-            console.log("no email found");
-            throw new Error(`email ${email} doesn't exist`);
+            console.log('no email found')
+            throw new Error(`email ${email} doesn't exist`)
         }
-        return Object.assign(new User(), results[0]);
+        let user = new User()
+        Object.assign(user, results[0])
+        return user;
     }
 
     static async getUserById(db_con, id) {
         const [results, fields] = await db_con.execute(
             'SELECT * FROM Users WHERE id = ?',
             [id]
-        );
+        )
         if (results.length == 0) {
-            throw new Error(`id ${id} doesn't exist`);
+            throw new Error(`id ${id} doesn't exist`)
         }
-        return Object.assign(new User(), results[0]);
-
+        let user = new User()
+        Object.assign(user, results[0])
+        return user;
     }
 }
 
 class UserAttendance {
-    constructor(user_id, time, photo_proof) {
-        this.user_id = user_id;
-        this.id = "";
-        this.time = time;
-        this.photo_proof = photo_proof;
+    constructor(user_id, time, photo_proof, check_in_out_type) {
+        this.user_id = user_id
+        this.id = null
+        this.time = time
+        this.photo_proof = photo_proof
+        this.check_in_out_type = check_in_out_type // checkin/checkout
+    }
+
+    async save(db_con) {
+        const [results, fields] = await db_con.execute(
+            'INSERT INTO UserAttendances(user_id, time, photo_proof, check_in_out_type) VALUES(?, ?, ?, ?)',
+            [this.user_id, this.time, this.photo_proof, this.check_in_out_type]
+        )
+        console.log(results)
     }
 }
 
 class UserSession {
     constructor(user_id, token) {
-        this.user_id = user_id;
-        this.id = "";
-        this.token = token;
+        this.user_id = user_id
+        this.id = null
+        this.token = token
     }
 
     async save(db_con) {
@@ -62,7 +89,7 @@ class UserSession {
         const [results, fields] = await db_con.execute(
             'INSERT INTO UserSessions(user_id, token) VALUES(?, ?)',
             [this.user_id, this.token]
-        );
+        )
         console.log(results)
     }
 
@@ -70,13 +97,13 @@ class UserSession {
         const [results, fields] = await db_con.execute(
             'SELECT user_id FROM UserSessions WHERE token = ?',
             [token]
-        );
-        if (results.length == 0){
-            throw new Error(`Token not valid`);
+        )
+        if (results.length == 0) {
+            throw new Error(`Token not valid`)
         }
-        let user_id = results[0].user_id;
-        return User.getUserById(db_con, user_id);
+        let user_id = results[0].user_id
+        return User.getUserById(db_con, user_id)
     }
 }
 
-module.exports = { User, UserAttendance, UserSession };
+module.exports = { User, UserAttendance, UserSession }
