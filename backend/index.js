ff67c1874e958a7b24d56eb41e3d490b911cb14d
kuhakupixel <nicholaspixels@gmail.com>
2025-07-11 10:30:32 +0700

    add login

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 
 const express = require('express')
 const app = express()
+const HttpStatus = require('http-status-codes');
 const port = 3005
 
 const mysql = require('mysql2/promise');
 var bodyParser = require('body-parser');
 const { User, UserAttendance } = require('./model');
+
 // parse application/json
 app.use(bodyParser.json());
 
@@ -34,24 +36,50 @@ app.use(bodyParser.json());
       req.body["email"],
       req.body["age"],
       req.body["password"],
+      true
+    );
+
+    try {
+      await user.save(connection);
+      res.send('account created');
+    }
+    catch (error) {
+      res.status(HttpStatus.StatusCodes.BAD_REQUEST).send(error["message"]);
+    }
+  })
+
+  app.post('/register-employee', async (req, res) => {
+    res.send('Hello World!')
+    console.log(req.body);
+    let user = new User(
+      req.body["username"],
+      req.body["email"],
+      req.body["age"],
+      req.body["password"],
       false
     );
 
     try {
       await user.save(connection);
-      res.send('account created')
+      res.send('account created');
     }
     catch (error) {
-      res.send(error["message"])
+      res.status(HttpStatus.StatusCodes.BAD_REQUEST).send(error["message"]);
     }
   })
 
-  app.post('/register-employee', (req, res) => {
-    res.send('Hello World!')
-  })
-
-  app.post('/login', (req, res) => {
-    res.send('Hello World!')
+  app.post('/login', async (req, res) => {
+    try {
+      let user = await User.getUserByEmail(connection, req.body["email"]);
+      if (user.password != req.body["password"]) {
+        res.send("wrong password");
+      }
+      res.send("ok");
+    }
+    catch (error) {
+      console.log("error is " + error);
+      res.status(HttpStatus.StatusCodes.BAD_REQUEST).send(error.toString());
+    }
   })
 
   app.get('/attendances', (req, res) => {
--- backend/model.js
+++ backend/model.js
@@ -3,6 +3,7 @@ class User {
         this.username = username;
         this.email = email;
         this.age = age;
+        this.id = "";
         this.password = password;
         this.is_admin = is_admin;
     }
@@ -14,6 +15,28 @@ class User {
             [this.email, this.username, this.age, this.password, this.is_admin]
         );
         console.log(results)
+    }
+    static async getUserByEmail(db_con, email) {
+        const [results, fields] = await db_con.execute(
+            'SELECT * FROM Users WHERE email = ?',
+            [email]
+        );
+        if (results.length == 0) {
+            console.log("no email found");
+            throw new Error(`email ${email} doesn't exist`);
+        }
+        return Object.assign(new User(), results[0]);
+    }
+
+    static async getUserById(db_con, id) {
+        const [results, fields] = await db_con.execute(
+            'SELECT * FROM Users WHERE id = ?',
+            [id]
+        );
+        if (results.length == 0) {
+            throw new Error(`id ${id} doesn't exist`);
+        }
+        return Object.assign(new User(), results[0]);
 
     }
 }
@@ -21,8 +44,39 @@ class User {
 class UserAttendance {
     constructor(user_id, time, photo_proof) {
         this.user_id = user_id;
+        this.id = "";
         this.time = time;
         this.photo_proof = photo_proof;
     }
 }
+
+class UserSession {
+    constructor(user_id, token) {
+        this.user_id = user_id;
+        this.id = "";
+        this.token = token;
+    }
+
+    async save(db_con) {
+        const [results, fields] = await db_con.execute(
+            'INSERT INTO UserSessions(user_id, token) VALUES(?, ?)',
+            [this.user_id, this.token]
+        );
+        console.log(results)
+    }
+
+    static async get_user(db_con, token) {
+        const [results, fields] = await db_con.execute(
+            'SELECT user_id FROM UserSessions WHERE token = ?',
+            [this.token]
+        );
+        if (results.length == 0){
+            throw new Error(`Token not valid`);
+        }
+        let user_id = results[0].user_id;
+        User.getUserByEmail
+        console.log()
+    }
+}
+
 module.exports = { User, UserAttendance };
