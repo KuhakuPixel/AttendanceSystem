3c93012887a62496185a1d6304582229c3074a92
kuhakupixel <nicholaspixels@gmail.com>
2025-07-14 08:28:19 +0700

    remove log

--- backend/auth.js
+++ backend/auth.js
@@ -4,7 +4,6 @@ async function requireLogin(db_con, req, is_admin_required) {
     if (token == null){
         throw new Error("Auth token not provided in authorization");
     }
-    console.log("token is " + token);
     let user = await UserSession.getUser(db_con, token)
     if (is_admin_required && !user.is_admin){
         throw new Error("user must be an admin");
